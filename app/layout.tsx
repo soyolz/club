1831932cf91062ac0aa7f319b3ad11d96129ac3d
SOYOLZ <fritzfils7aime@gmail.com>
2025-09-19 20:12:01 -0400

    New

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const xboxFont= localFont({
+  src: './fonts/Conv.ttf'
 });
 
 export const metadata: Metadata = {
@@ -25,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={xboxFont.className}
       >
         {children}
       </body>
